17b9513fb27034a780251dce7ef0157e0d88f7b8
jerry-lllman <jerry_lllman@163.com>
2022-08-15 17:13:47 +0800

    chore: 全局异常拦截添加

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -21,6 +23,12 @@ async function bootstrap() {
 
   // 统一响应体格式
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  // 异常过滤器
+  app.useGlobalFilters(
+    new AllExceptionsFilter(),
+    new HttpExceptionFilter()
+  );
 
   await app.listen(3000);
 }
