ecac65866c2eaac515f8ba46de9a070e606ec1f4
jerry-lllman <jerry_lllman@163.com>
2022-08-15 16:58:48 +0800

    chore: 启用版本控制

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -10,6 +11,12 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter()
   );
+
+  // 接口版本化管理
+  app.enableVersioning({
+    type: VersioningType.URI
+  })
+
   await app.listen(3000);
 }
 bootstrap();
